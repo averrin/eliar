7039520697962fb919227c213ba92777539187d5
Averrin <averrin@gmail.com>
2011-07-21 17:47:05 +0400

    encoding hell

--- areal/urls.py
+++ areal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
-from areal.views import index
+from areal.views import index, get_content
 
 urlpatterns = patterns('',
                        url(r'^$', index),
+                       url(r'^getcontent/(?P<msg_id>\d+)$', get_content),
                        )
--- areal/views.py
+++ areal/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
 
 from shared import *
@@ -8,74 +9,100 @@ import email
 import chardet
 
 
+def get_connection():
+    c=imaplib.IMAP4_SSL('imap.yandex.ru')
+    c.login('arealtr99@yandex.ru','201965')
+    c.select('INBOX', readonly=True)
+    print 'connected'
+    return c
+
+def get_last_id(c):
+    typ, msg_ids = c.search(None, 'ALL')
+    last=msg_ids[0].split(' ')[-1]
+    return last
+
+def fetch_msg(c,id):
+    typ, msg_data = c.fetch(id, '(RFC822)')
+    print 'fetch %s [%s]' % (id,typ)
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            msg = email.message_from_string(response_part[1])
+            subj=email.Header.decode_header(msg['subject'])[0]
+            s=subj[0]
+            if not subj[1] is None:
+                s=s.decode(subj[1])
+            msg['subj_e']=s
+            subj=email.Header.decode_header(msg['from'])[0]
+            f=subj[0]
+            if not subj[1] is None:
+                f=f.decode(subj[1])
+            msg['from_e']=f
+            msg['id']=id
+            msg['text_e']=u'Для получения содержимого письма нажмите кнопку "Текст"'
+    return msg
+
+def get_from_mp(msg):
+    for part in msg.get_payload():
+        print '%s -- %s' % (msg['id'],part.get_content_maintype())
+        if part.get_content_maintype() == 'text':
+            text=part.get_payload(decode=True)
+        else:
+            if part.is_multipart():
+                text=get_from_mp(part)
+    return text
+
+def get_msg_content(msg):
+    from BeautifulSoup import BeautifulSoup
+    text=''
+    last=msg['id']
+    if msg.is_multipart():
+        text=get_from_mp(msg)
+    else:
+        text=msg.get_payload(decode=True)
+
+    text=quopri.decodestring(text)
+    enc=BeautifulSoup(text).originalEncoding
+    msg['enc']=enc
+    subre="<b><span style='font-weight:bold'>Subject:</span></b> Re:"
+    print msg['subj_e']
+
+
+    if enc and enc in ['ISO-8859-2']:
+        try:
+            msg['text']=text.decode('cp1251')
+        except:
+            msg['text']=text.replace('\x98','').decode('cp1251')
+    elif enc and enc not in ['utf8','utf-8']:
+        msg['text']=text.decode(enc)
+    else:
+        msg['text']=text
+
+    return msg['text']
+
+
+
+
 @render_to('areal/index.html')
 def index(request):
-    c=imaplib.IMAP4_SSL('imap.yandex.ru')
-    print 'connection'
-    c.login('arealtr99@yandex.ru','201965')
-    print 'logged'
-    c.select('INBOX', readonly=True)
-    print 'selected'
-    typ, msg_ids = c.search(None, 'ALL')
-    print '='*8
-    last=msg_ids[0].split(' ')[-1]
+    c=get_connection()
+    last=get_last_id(c)
+
     resp=[]
     for i in xrange(8):
-        typ, msg_data = c.fetch(last, '(RFC822)')
-        print 'fetch %s [%s]' % (last,typ)
-        for response_part in msg_data:
-            if isinstance(response_part, tuple):
-                msg = email.message_from_string(response_part[1])
-    #            msg.set_charset('koi8-r')
-                subj=email.Header.decode_header(msg['subject'])[0]
-                s=subj[0]
-                if not subj[1] is None:
-                    s=s.decode(subj[1])
-                msg['subj_e']=s
-                subj=email.Header.decode_header(msg['from'])[0]
-                f=subj[0]
-                if not subj[1] is None:
-                    f=f.decode(subj[1])
-                msg['from_e']=f
-                msg['id']=last
-                text=''
-                if msg.is_multipart():
-                    for part in msg.get_payload():
-                        print '%s -- %s' % (last,part.get_content_maintype())
-                        if part.get_content_maintype() == 'text':
-                            text=part.get_payload(decode=True)
-                        elif part.is_multipart():
-                            for pp in part.get_payload():
-                                print '-- %s -- %s' % (last,pp.get_content_maintype())
-                                if pp.get_content_maintype() == 'text':
-                                    text=pp.get_payload(decode=True)
-#                            if not text:
-#                                text=part.get_payload()
-                else:
-                    text=msg.get_payload(decode=True)
-
-#                text=msg.get_payload()[1].get_payload()
-                text=quopri.decodestring(text)
-                enc=chardet.detect(text)['encoding']
-#                print '%s: %s (%s)' % (last,text[0:60],enc)
-
-                if enc and enc in ['ISO-8859-2'] and not msg['subj_e'].startswith('RE:'):
-                    msg['text_e']=text.decode('cp1251')
-                elif enc and enc not in ['utf8','utf-8']:
-                    if msg['subj_e'].startswith('RE:'):
-                        msg['text_e']=text.decode('koi8-r')
-                    else:
-                        msg['text_e']=text.decode(enc)
-                else:
-                    msg['text_e']=text
-
-
-
-                resp.append(msg)
+        msg=fetch_msg(c,last)
+        resp.append(msg)
         last=str(int(last)-1)
     c.logout()
 
 
     return {'msgs':resp,'response':''}
 
-
+def get_content(request,msg_id):
+    c=get_connection()
+    msg=fetch_msg(c,msg_id)
+    c.logout()
+    try:
+        text=get_msg_content(msg)
+    except Exception, e:
+        text=u'Опять кодировка какая-то левая=((<br> encoding: %s<br>Error: %s' % (msg['enc'],str(e))
+    return HttpResponse(text+'<br>Encoding: '+msg['enc'])
